dce5e6dda2203de5bb1c9bc70ea23b239dea3ea8
springhack <springhack@live.cnb>
2016-08-06 22:49:49 +0800

    Add 404, fix path

---
+++ 404/webpack.config.js
@@ -0,0 +1,47 @@
+var webpack = require('webpack');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    index : './src/index.js'
+  },
+  output: {
+    path: 'dist',
+    filename: './js/[name].js'
+  },
+  resolve: {
+    
+  },
+  module: {
+    loaders: [{
+      test: /\.js(x)?$/,
+      loader: 'babel-loader',
+      exclude: function (path) {
+        return (!!path.match(/node_modules/));
+      },
+      query: {
+        presets:['react','es2015']
+      }
+    },
+    {
+      test: /\.css$/,
+      loader: ExtractTextPlugin.extract(['css'])
+    },
+    {
+      test: /\.less$/,
+      loader: ExtractTextPlugin.extract(['css', 'less'])
+    }]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'SpringHack',
+      inject: true,
+      minify: {
+        removeComments: true,
+	    collapseWhitespace: false
+      }
+    }),
+    new ExtractTextPlugin("./css/[name].css")
+  ]
+};
