3c36a22698a3d512dee2e84f645b635acea39960
springhack <springhack@live.cnb>
2016-09-25 15:56:51 +0800

    Fix login  api

--- src/jsx/Login.jsx
+++ src/jsx/Login.jsx
@@ -25,7 +25,7 @@ export default class Login extends React.Component {
     onLogin() {
         let user = ReactDOM.findDOMNode(this.refs.user).getElementsByTagName('input')[0].value;
         let pass = ReactDOM.findDOMNode(this.refs.pass).getElementsByTagName('input')[0].value;
-        fetch('Login', {
+        fetch('/user/login', {
             method : 'POST',
             credentials : 'include',
             headers: {
@@ -41,20 +41,21 @@ export default class Login extends React.Component {
                     content : 'Network error !'
                 });
         }).then(json => {
-                if (json.result == 'success')
-                {
-                    this.setState({
-                        display : 'block',
-                        content : 'Success'
-                    });
-                    setTimeout(function () {
-                        location.href = 'Main';
-                    }, 1500);
-                } else
-                    this.setState({
-                        display : 'block',
-                        content : json.reason
-                    });
+            if (json.error)
+            {
+                this.setState({
+                    display : 'block',
+                    content : json.error
+                });
+            } else {
+                this.setState({
+                    display : 'block',
+                    content : 'Success'
+                });
+                setTimeout(function () {
+                    location.href = 'Main';
+                }, 1500);
+            }
         }).catch(err => {
             console.log(err);
         });
